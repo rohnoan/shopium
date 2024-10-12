d895130cc0816a9f3a63be61cf65de4b687b1901
rohan <sharmarohan2507@gmail.com>
2024-10-13 05:29:35 +0530

    initial+tailwind

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+
+export default function App() {
+  return (
+    <div className='text-2xl'>
+    <Navbar/>
+    </div>
+  )
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './images/logo.png'
+export default function Navbar() {
+  return (
+    <div className='h-[100px] bg-blue-900'>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
