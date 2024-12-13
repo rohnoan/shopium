c29abe161d8e6402a9b16e45560795cc315387c2
rohan <sharmarohan2507@gmail.com>
2024-12-13 17:04:03 +0530

    resolved errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,26 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import SearchBar from './components/SearchBar'
-import Products from './pages/Products'
-import MegaSale from './components/MegaSale'
-import DesigningFlex from './components/DesigningFlex'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contact from './pages/Contact'
-import Cart from './pages/Cart'
-import NotFound from './pages/NotFound'
-import Footer from './components/Footer'
-import ProductBlock from './components/ProductBlock'
-import CartContextProvider from './context/CartContextProvider'
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import Contact from './pages/Contact';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+import Footer from './components/Footer';
+import CartContextProvider from './context/CartContextProvider';
+
 export default function App() {
   return (
     <CartContextProvider>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shopium" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Router basename="/shopium"> {/* Ensure this matches your repo name */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
       <Footer />
     </CartContextProvider>
   );
